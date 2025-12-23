c1f3309c60f84ffc285e9fc50abecdbddb96a42f
ALi Rezaei <im1.bitcode@gmail.com>
2025-12-23 12:51:13 +0330

    add link to homepage

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { Link } from "react-router";
+
 export default function HomePage() {
-  return <div>Home</div>;
+  return (
+    <div className="h-svh flex items-center justify-center">
+      <Button variant="outline" asChild>
+        <Link to="/vendors">مراکز فروش</Link>
+      </Button>
+    </div>
+  );
 }
